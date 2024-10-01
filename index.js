a6d0e40b3da0c22a2bb9bded7982bedd5c2dd0eb
sayed udoy <umudoy364@gmail.com>
2024-10-01 15:50:03 +0500

    some changes

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
 
 
-// selector 
-const hamBtn = document.getElementById("hamBtn")
-const hamMenu = document.getElementById("hamMenu")
-const hideHamMenu = document.getElementById("hideHamMenu")
-// addEventListener 
-hamBtn.addEventListener("click",showhamMenu)
-hideHamMenu.addEventListener("click",hideHamBurgerMenu)
-// addEventListener Function 
-function showhamMenu () {
-  hamMenu.classList.toggle("-translate-x-[1000px]")
-}
-function hideHamBurgerMenu (){
-  hamMenu.classList.toggle("-translate-x-[1000px]")
-}
 
